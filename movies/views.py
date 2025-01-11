090a08cb7dd872302162388ecda9617461188fb0
Abrahan MAsabanda <jamasabanda@puce.edu.ec.>
2025-01-11 10:56:03 -0500

    Examen 2

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
+
+def movie_detail(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
